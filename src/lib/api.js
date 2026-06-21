55bbafa9807f327ef1bf9dca6684c35a0c7dec77
RiviniOpenARC <rivinisanduni@gmail.com>
2026-06-21 17:03:29 +0530

    docker file add

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,9 +4,8 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 })
 
-// Attach token automatically
+// Request interceptor
 api.interceptors.request.use((config) => {
-
   const token = localStorage.getItem('token')
 
   if (token) {
@@ -15,5 +14,51 @@ api.interceptors.request.use((config) => {
 
   return config
 })
+
+// Response interceptor
+api.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const originalRequest = error.config
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true
+
+      try {
+        const token = localStorage.getItem('token')
+
+        const refreshResponse = await axios.post(
+          `${import.meta.env.VITE_API_URL}/refresh`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          }
+        )
+
+        const newToken = refreshResponse.data.access_token
+
+        localStorage.setItem('token', newToken)
+
+        originalRequest.headers.Authorization =
+          `Bearer ${newToken}`
+
+        return api(originalRequest)
+      } catch (refreshError) {
+        localStorage.removeItem('token')
+        window.location.href = '/login'
+
+        return Promise.reject(refreshError)
+      }
+    }
+
+    return Promise.reject(error)
+  }
+)
 
 export default api
